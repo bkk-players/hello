7080d272759ac94a9be5a2693fd51aaab352e732
mario-viegash <mario.viegash@traveloka.com>
2020-05-28 20:32:41 +0700

    Updates

---
+++ precache-manifest.ebd33dfac42f6d05239cc3429f8fa569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9c4691f862b67d702b9f866dab629a",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "3fca04eee0ced93484e7",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dd144ce01c7461613985",
+    "url": "/hello/static/js/2.18b3a103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.18b3a103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fca04eee0ced93484e7",
+    "url": "/hello/static/js/main.b1c05c61.chunk.js"
+  },
+  {
+    "revision": "cc1a559921ccb19b7656",
+    "url": "/hello/static/js/runtime-main.44fbf7b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
